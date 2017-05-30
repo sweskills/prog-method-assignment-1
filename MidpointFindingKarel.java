3a3294e87dba677525e92da89c4d9e1a0787daf2
S <s>
2017-05-30 23:07:16 +0100

    {"types":"C","runs":34}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -13,7 +13,127 @@
 import stanford.karel.*;
 
 public class MidpointFindingKarel extends SuperKarel {
+		public void run() {
+			while (notAtTheTop()){ 
+				fillRow();
+				goToNextRow();
+			}
+			fillRow();
+			faceNorth();
+		}
 
-	// You fill in this part
+	private void fillRow() {
+		faceEast();
+		if (frontIsBlocked()){
+			faceWest();
+		}
+		placeSpacedBeeper();
+	}
+
+	private void placeSpacedBeeper() {
+		while (frontIsClear()){
+		 placeOneBeeper();
+		 for ( int i=1; i<=2; i++){
+			 if (frontIsClear()){
+				 moveMultiple(1);
+			 } 
+		 }
+		}
+	}
+
+	private void goToNextRow() {
+		faceNorth();
+		move();
+		faceEast();
+		if (frontIsBlocked()){
+			faceWest();
+		}
+		
+	}
+
+		private boolean notAtTheTop() {
+			faceNorth();
+			if (frontIsBlocked()){
+				return false;
+			}
+			else { 
+				return true;
+			}
+		}
+		 
+		 private void placeOneBeeper(){
+			 if (noBeepersPresent()){
+				 putBeeper();
+			 }
+		 }
+		 
+		 private void moveMultiple(int n) {
+			for (int i=1; i<=n; i++){
+				move();
+			}
+		}
+
+		private void moveToWall(){
+		    	while (frontIsClear()){
+		    		moveMultiple(1);
+		    	}
+		    }
+		
+	    private void faceSouth(){
+	    	
+			if (facingEast()){
+				turnMultipleLeft(3);
+			}
+			else if (facingNorth()){
+				turnMultipleLeft(2);
+			}
+			else if (facingWest()){
+				turnMultipleLeft(1);
+			}
+	    }
+	    
+	    private void faceEast(){
+			if (facingNorth()){
+				turnMultipleLeft(3);
+			}
+			else if (facingSouth()){
+				turnMultipleLeft(1);
+			}
+			else if (facingWest()){
+				turnMultipleLeft(2);
+			}
+	    }
+	    
+	    private void faceNorth(){
+			if (facingEast()){
+				turnMultipleLeft(1);
+			}
+			else if (facingSouth()){
+				turnMultipleLeft(2);
+			}
+			else if (facingWest()){
+				turnMultipleLeft(3);
+			}
+	    }
+	    
+	    private void faceWest(){
+			if (facingEast()){
+				turnMultipleLeft(2);
+			}
+			else if (facingSouth()){
+				turnMultipleLeft(3);
+			}
+			else if (facingNorth()){
+				turnMultipleLeft(1);
+			}
+	    }
+	    
+	    private void turnMultipleLeft(int n){
+	    	for (int i=1; i<=n; i++){
+	    		turnLeft();
+	    	}
+	    }
+
+
 
 }
