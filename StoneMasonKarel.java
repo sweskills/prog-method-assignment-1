85664d62b290702acdc386875468db2471f086d2
S <s>
2017-05-30 23:35:47 +0100

    {"types":"C","runs":47}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -1,3 +1,4 @@
+
 /*
  * File: StoneMasonKarel.java
  * --------------------------
@@ -12,104 +13,96 @@ import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
 
-	public void run () {
+	public void run() {
 		fillColumn();
-		while (frontIsClear()){ 
-		goToNextColumn();
-		fillColumn();
+		while (frontIsClear()) {
+			goToNextColumn();
+			fillColumn();
 		}
 	}
-	
-	 private void fillColumn(){
-		faceNorth();	
-//		if (frontIsBlocked()){
-//			faceSouth();
-//		}
-		 while (frontIsClear()){
-			 placeOneBeeper();
-			 move();
-		 }
-		 placeOneBeeper();
-		 faceSouth();
-		 moveToWall();
-		 faceEast();
-	 }
-	 
-	 private void placeOneBeeper(){
-		 if (noBeepersPresent()){
-			 putBeeper();
-		 }
-	 }
-	 
-	 private void goToNextColumn(){
-		 moveMultiple(4);
-	 }
-	 
-	 private void moveMultiple(int n) {
-		for (int i=1; i<=n; i++){
+
+	private void fillColumn() {
+		faceNorth();
+		// if (frontIsBlocked()){
+		// faceSouth();
+		// }
+		while (frontIsClear()) {
+			placeOneBeeper();
+			move();
+		}
+		placeOneBeeper();
+		faceSouth();
+		moveToWall();
+		faceEast();
+	}
+
+	private void placeOneBeeper() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+	}
+
+	private void goToNextColumn() {
+		moveMultiple(4);
+	}
+
+	private void moveMultiple(int n) {
+		for (int i = 1; i <= n; i++) {
 			move();
 		}
 	}
 
-	private void moveToWall(){
-	    	while (frontIsClear()){
-	    		moveMultiple(1);
-	    	}
-	    }
-	
-    private void faceSouth(){
-    	
-		if (facingEast()){
+	private void moveToWall() {
+		while (frontIsClear()) {
+			moveMultiple(1);
+		}
+	}
+
+	private void faceSouth() {
+
+		if (facingEast()) {
 			turnMultipleLeft(3);
-		}
-		else if (facingNorth()){
+		} else if (facingNorth()) {
 			turnMultipleLeft(2);
-		}
-		else if (facingWest()){
+		} else if (facingWest()) {
 			turnMultipleLeft(1);
 		}
-    }
-    
-    private void faceEast(){
-		if (facingNorth()){
+	}
+
+	private void faceEast() {
+		if (facingNorth()) {
 			turnMultipleLeft(3);
-		}
-		else if (facingSouth()){
+		} else if (facingSouth()) {
 			turnMultipleLeft(1);
-		}
-		else if (facingWest()){
+		} else if (facingWest()) {
 			turnMultipleLeft(2);
 		}
-    }
-    
-    private void faceNorth(){
-		if (facingEast()){
+	}
+
+	private void faceNorth() {
+		if (facingEast()) {
 			turnMultipleLeft(1);
-		}
-		else if (facingSouth()){
+		} else if (facingSouth()) {
 			turnMultipleLeft(2);
-		}
-		else if (facingWest()){
+		} else if (facingWest()) {
 			turnMultipleLeft(3);
 		}
-    }
-    
-    private void faceWest(){
-		if (facingEast()){
+	}
+
+	private void faceWest() {
+		if (facingEast()) {
 			turnMultipleLeft(2);
-		}
-		else if (facingSouth()){
+		} else if (facingSouth()) {
 			turnMultipleLeft(3);
-		}
-		else if (facingNorth()){
+		} else if (facingNorth()) {
 			turnMultipleLeft(1);
 		}
-    }
-    
-    private void turnMultipleLeft(int n){
-    	for (int i=1; i<=n; i++){
-    		turnLeft();
-    	}
-    }
+	}
+
+	private void turnMultipleLeft(int n) {
+		for (int i = 1; i <= n; i++) {
+			turnLeft();
+		}
+	}
 
 }
