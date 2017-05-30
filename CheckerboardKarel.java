ead2e78e8d41a850036f7ff1dc9ec59f1b513af8
S <s>
2017-05-30 23:37:10 +0100

    {"types":"C","runs":47}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -1,3 +1,4 @@
+
 /*
  * File: CheckerboardKarel.java
  * ----------------------------
@@ -12,7 +13,7 @@ import stanford.karel.*;
 public class CheckerboardKarel extends SuperKarel {
 
 	public void run() {
-		while (notAtTheTop()){ 
+		while (notAtTheTop()) {
 			fillRow();
 			goToNextRow();
 		}
@@ -20,117 +21,107 @@ public class CheckerboardKarel extends SuperKarel {
 		faceNorth();
 	}
 
-private void fillRow() {
-	faceEast();
-	if (frontIsBlocked()){
-		faceWest();
+	private void fillRow() {
+		faceEast();
+		if (frontIsBlocked()) {
+			faceWest();
+		}
+		placeSpacedBeeper();
 	}
-	placeSpacedBeeper();
-}
 
-private void placeSpacedBeeper() {
-	while (frontIsClear()){
-	 placeOneBeeper();
-	 for ( int i=1; i<=2; i++){
-		 if (frontIsClear()){
-			 moveMultiple(1);
-		 } 
-	 }
+	private void placeSpacedBeeper() {
+		while (frontIsClear()) {
+			placeOneBeeper();
+			for (int i = 1; i <= 2; i++) {
+				if (frontIsClear()) {
+					moveMultiple(1);
+				}
+			}
+		}
 	}
-}
 
-private void goToNextRow() {
-	faceNorth();
-	move();
-	faceEast();
-	if (frontIsBlocked()){
-		faceWest();
+	private void goToNextRow() {
+		faceNorth();
+		move();
+		faceEast();
+		if (frontIsBlocked()) {
+			faceWest();
+		}
+
 	}
-	
-}
 
 	private boolean notAtTheTop() {
 		faceNorth();
-		if (frontIsBlocked()){
+		if (frontIsBlocked()) {
 			return false;
-		}
-		else { 
+		} else {
 			return true;
 		}
 	}
-	 
-	 private void placeOneBeeper(){
-		 if (noBeepersPresent()){
-			 putBeeper();
-		 }
-	 }
-	 
-	 private void moveMultiple(int n) {
-		for (int i=1; i<=n; i++){
+
+	private void placeOneBeeper() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+	}
+
+	private void moveMultiple(int n) {
+		for (int i = 1; i <= n; i++) {
 			move();
 		}
 	}
 
-	private void moveToWall(){
-	    	while (frontIsClear()){
-	    		moveMultiple(1);
-	    	}
-	    }
-	
-    private void faceSouth(){
-    	
-		if (facingEast()){
-			turnMultipleLeft(3);
+	private void moveToWall() {
+		while (frontIsClear()) {
+			moveMultiple(1);
 		}
-		else if (facingNorth()){
-			turnMultipleLeft(2);
-		}
-		else if (facingWest()){
-			turnMultipleLeft(1);
-		}
-    }
-    
-    private void faceEast(){
-		if (facingNorth()){
-			turnMultipleLeft(3);
-		}
-		else if (facingSouth()){
-			turnMultipleLeft(1);
-		}
-		else if (facingWest()){
-			turnMultipleLeft(2);
-		}
-    }
-    
-    private void faceNorth(){
-		if (facingEast()){
-			turnMultipleLeft(1);
-		}
-		else if (facingSouth()){
-			turnMultipleLeft(2);
-		}
-		else if (facingWest()){
-			turnMultipleLeft(3);
-		}
-    }
-    
-    private void faceWest(){
-		if (facingEast()){
-			turnMultipleLeft(2);
-		}
-		else if (facingSouth()){
-			turnMultipleLeft(3);
-		}
-		else if (facingNorth()){
-			turnMultipleLeft(1);
-		}
-    }
-    
-    private void turnMultipleLeft(int n){
-    	for (int i=1; i<=n; i++){
-    		turnLeft();
-    	}
-    }
+	}
 
+	private void faceSouth() {
+
+		if (facingEast()) {
+			turnMultipleLeft(3);
+		} else if (facingNorth()) {
+			turnMultipleLeft(2);
+		} else if (facingWest()) {
+			turnMultipleLeft(1);
+		}
+	}
+
+	private void faceEast() {
+		if (facingNorth()) {
+			turnMultipleLeft(3);
+		} else if (facingSouth()) {
+			turnMultipleLeft(1);
+		} else if (facingWest()) {
+			turnMultipleLeft(2);
+		}
+	}
+
+	private void faceNorth() {
+		if (facingEast()) {
+			turnMultipleLeft(1);
+		} else if (facingSouth()) {
+			turnMultipleLeft(2);
+		} else if (facingWest()) {
+			turnMultipleLeft(3);
+		}
+	}
+
+	private void faceWest() {
+		if (facingEast()) {
+			turnMultipleLeft(2);
+		} else if (facingSouth()) {
+			turnMultipleLeft(3);
+		} else if (facingNorth()) {
+			turnMultipleLeft(1);
+		}
+	}
+
+	private void turnMultipleLeft(int n) {
+		for (int i = 1; i <= n; i++) {
+			turnLeft();
+		}
+	}
 
 }
