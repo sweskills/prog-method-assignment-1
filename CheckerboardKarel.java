40e2aec8ca555c8f3a0374431ba32b1a4456cd2e
S <s>
2017-05-30 22:33:00 +0100

    {"types":"C","runs":25}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -46,12 +46,6 @@ private void fillEastWards(){
 	faceEast();
 	while (frontIsClear()){
 		 placeSpacedBeeper();
-		 if (frontIsClear()){
-			 moveMultiple(2);
-		 }
-		 else{
-			 moveMultiple(1);
-		 }
 	 }
 }
 
