25dce946a68299b30a0fb65d29b6e959d0a7c4ee
S <s>
2017-05-31 13:36:41 +0100

    {"types":"C","runs":113}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -18,11 +18,7 @@ public class CheckerboardKarel extends SuperKarel {
 		
 		//Karel move in two steps and drop beeper and then move to another row
 		beeperDropping();
-		if (frontIsClear() )	{
-			move();
-		}
-
-		
+		madeEasy();
 		
 		//Karel move in two steps and drop beeper and then move to another row
 		beeperDropping();
