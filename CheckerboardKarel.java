a02b5dbc8b068fc28eb7c6ea307638d27e12b3c0
S <s>
2017-05-31 13:12:24 +0100

    {"types":"C","runs":72}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -19,7 +19,7 @@ public class CheckerboardKarel extends SuperKarel {
 	private void navigate() {
 		
 		while (!frontIsBlocked()) {
-			for(int i=0; i<3;i++){
+			for(int i=0; i<2;i++){
 			move();
 			}
 			putBeeper();
