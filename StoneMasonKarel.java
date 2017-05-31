d0acaf090570e51b090b99e6814b5ee227b1cf31
S <s>
2017-05-31 13:18:25 +0100

    {"types":"C","runs":95}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -43,6 +43,7 @@ public class StoneMasonKarel extends SuperKarel {
 		//
 		turnLeft();
 		moveDoubleDouble();
+		turnRight();
 		while (frontIsClear() )	{
 			if (noBeepersPresent() ) {
 				putBeeper();
