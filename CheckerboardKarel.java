6c82780d3f848a0809a51bdb88d8d5c5423a8a12
S <s>
2017-05-31 23:53:56 +0100

    {"types":"C","runs":59}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -14,7 +14,7 @@ public class CheckerboardKarel extends SuperKarel {
 	// You fill in this part
 	public void run(){
 		putBeeper();
-		while (frontIsClear()){
+		if (frontIsClear()){
 			move();
 			move();
 			putBeeper();
