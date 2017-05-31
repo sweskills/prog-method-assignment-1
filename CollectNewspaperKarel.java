c4f55903b9203607631e82521d5c11ed148df64b
S <s>
2017-05-31 12:21:04 +0100

    {"types":"C","runs":11}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -1,30 +1,3 @@
-/*
- * File: CollectNewspaperKarel.java
- * --------------------------------
- * At present, the CollectNewspaperKarel subclass does nothing.
- * Your job in the assignment is to add the necessary code to
- * instruct Karel to walk to the door of its house, pick up the
- * newspaper (represented by a beeper, of course), and then return
- * to its initial position in the upper left corner of the house.
- */
-
-/*import stanford.karel.*;
-
-public class CollectNewspaperKarel extends SuperKarel {
-
-	// You fill in this part
-
-}
-/*
- * File: CollectNewspaperKarel.java
- * --------------------------------
- * At present, the CollectNewspaperKarel subclass does nothing.
- * Your job in the assignment is to add the necessary code to
- * instruct Karel to walk to the door of its house, pick up the
- * newspaper (represented by a beeper, of course), and then return
- * to its initial position in the upper left corner of the house.
- */
-
 import stanford.karel.*;
 
 public class CollectNewspaperKarel extends SuperKarel {
@@ -70,7 +43,7 @@ public class CollectNewspaperKarel extends SuperKarel {
      turnMultipleLeft(5);
     }
     
-    private void FaceNorth() {
+    private void faceNorth(){
      
     }
     
