17784ab18dc388bdfa291c8721e29d7a7f30d382
S <s>
2017-06-11 13:50:25 +0100

    {"types":"C","runs":74}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -20,8 +20,8 @@ public class MidpointFindingKarel extends SuperKarel {
 			takeLastBeeperEast();
 			takeLastBeeperWest();
 		}
-		turnLeft();
-	
+		turnAround();
+		turnAround();
     }
     
 	private void putEndBeepers() {
